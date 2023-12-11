b919d617ac139a8c651cfc00b9f70af5a5684dcf
linedubeth62 <153516253+linedubeth62@users.noreply.github.com>
2023-12-11 15:09:05 +0100

    Update jeu_de_carte_multiplication.py

--- jeu_de_carte_multiplication.py
+++ jeu_de_carte_multiplication.py
@@ -1,4 +1,48 @@
-déclarations des variables
+/* déclarations des variables
+*/
 
-nb_joeurs
-nb_cartes
+nb_joueurs=[2..5]
+nb_cartes=41
+nb_joker=4
+pioche=0
+
+/* première règle
+*/
+
+Au départ on a des nombres compris en 2 et 100
+on crée la tables nombres
+
+1
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+2
+
+4
+6
+8
+10
+12
+14
+16
+18
+20
+
+......
+10
+20
+30...100
+
+
+
+
+
+
